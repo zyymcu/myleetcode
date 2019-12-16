f2f1a960de78e4206d633620b8ad499f823ecb43
Rafael Zhou <zyymcu@qq.com>
2019-12-16 17:15:50 +0800

    add 108

---
+++ 108_tobintree/108_tobintree.cpp
@@ -0,0 +1,102 @@
+// 108_tobintree.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <string>
+#include <set>
+#include <unordered_set>
+#include <cassert>
+#include <unordered_map>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return helper(nums, 0, nums.size() - 1);
+    }
+    TreeNode* helper(vector<int>& nums,int left,int right) {
+        if (right < left ) { 
+            return nullptr; 
+        }
+        int mid = (right-left)/2+left;   
+        TreeNode* root = new TreeNode(nums[mid]);
+        root->left =  helper(nums, left, mid - 1);
+        root->right = helper(nums, mid+1, right);
+        return root;
+    }
+};
+
+void trimLeftTrailingSpaces(string& input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+        }));
+}
+
+void trimRightTrailingSpaces(string& input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+        }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+string treeNodeToString(TreeNode* root) {
+    if (root == nullptr) {
+        return "[]";
+    }
+
+    string output = "";
+    queue<TreeNode*> q;
+    q.push(root);
+    while (!q.empty()) {
+        TreeNode* node = q.front();
+        q.pop();
+
+        if (node == nullptr) {
+            output += "null, ";
+            continue;
+        }
+
+        output += to_string(node->val) + ", ";
+        q.push(node->left);
+        q.push(node->right);
+    }
+    return "[" + output.substr(0, output.length() - 2) + "]";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        vector<int> nums = stringToIntegerVector(line);
+
+        TreeNode* ret = Solution().sortedArrayToBST(nums);
+
+        string out = treeNodeToString(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
+
